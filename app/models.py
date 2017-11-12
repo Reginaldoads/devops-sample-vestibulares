fd2c019fe27c789a39be3dc3565f2a69c9dc9744
Reginaldoads <30833357+Reginaldoads@users.noreply.github.com>
2017-11-12 13:24:26 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ Definition of models.
 from django.db import models
 
 class Curso(models.Model):
-  nome = models.CharFiel(max_length=200)
+  nome = models.CharField(max_length=200)
   periodo = models.CharField(max_length=50)
   instituicao = models.CharField(max_length=200)
 
